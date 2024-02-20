515b0a664ba4c9fdbb033c73dcc14c7506c50a69
desniland <albertdesmond26@gmail.com>
2024-02-20 12:47:04 +0300

    function that sorts a deck of cards

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
- * swap - the positions of two elements into an array
- * @array: array
+ * swap - the positions of two elements in an array
+ * @array: the array
  * @item1: array element
  * @item2: array element
  */
@@ -18,7 +18,7 @@ void swap(int *array, ssize_t item1, ssize_t item2)
  * @array: array
  * @first: first array element
  * @last: last array element
- * @size: size array
+ * @size: size of the array
  * Return: return the position of the last element sorted
  */
 int hoare_partition(int *array, int first, int last, int size)
@@ -42,8 +42,8 @@ int hoare_partition(int *array, int first, int last, int size)
 	}
 }
 /**
- * qs - qucksort algorithm implementation
- * @array: array
+ * qs - impliments qucksort algorithm
+ * @array: the array
  * @first: first array element
  * @last: last array element
  * @size: array size
@@ -59,9 +59,10 @@ void qs(int *array, ssize_t first, ssize_t last, int size)
 		qs(array, position, last, size);
 	}
 }
+
 /**
- * quick_sort_hoare - prepare the terrain to quicksort algorithm
- * @array: array
+ * quick_sort_hoare - prepares the terrain to quicksort algorithm
+ * @array: the array
  * @size: array size
 */
 void quick_sort_hoare(int *array, size_t size)
